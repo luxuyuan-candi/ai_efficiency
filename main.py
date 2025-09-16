801cfe56c9b181bc31f60f498b6cc8fe41db5fa5
root <root@localhost.localdomain>
2025-09-16 22:58:19 +0800

    convert prompt

--- main.py
+++ main.py
@@ -32,7 +32,10 @@ async def main():
 你是一个运维小助手.
 根据用户提出的问题，尽量使用的工具，回答问题.
 回答的内容的样式如下，其中"返回的结果"保持原始内容，并使用markdown格式：
-<u>**{查询的子对象1}**</u>
+### 一、项目基本情况
+{项目信息}
+### 二、项目集群状态
+#### {查询的子对象1}
 **返回的结果**
 ```
 {原始内容}
@@ -40,7 +43,7 @@ async def main():
 **分析的结论**
 {结论}
 ---
-<u>**{查询的子对象2}**</u>
+#### {查询的子对象2}
 **返回的结果**
 ```
 {原始内容}
@@ -50,7 +53,7 @@ async def main():
 ---
 ...
 ---
-<u>**{查询的子对象n}**</u>
+#### {查询的子对象n}
 **返回的结果**
 ```
 {原始内容}
@@ -58,7 +61,9 @@ async def main():
 **分析的结论**
 {结论}
 ---
-**最终总结**
+#### 最终总结
+### 三、项目业务情况
+{项目业务}
 """
     # 配置 MCP servers
     client = MultiServerMCPClient({
@@ -92,8 +97,14 @@ async def main():
 
     # 用 Agent 做一次调用
     #config = {"configurable": {"thread_id": "2"}}
+    info_project = "一见视觉平台是一款好的产品，需要查询相关文档"
+    case_project = "项目业务情况良好，需要查询相关数据库"
     response = await agent.ainvoke(
-        {"messages": [{"role": "user", "content": "检查项目集群的基本资源情况"}]},
+        {"messages": [
+            {"role": "user", "content": "项目信息为:"+info_project},
+            {"role": "user", "content": "项目业务为:"+case_project},
+            {"role": "user", "content": "检查该项目"}
+            ]},
         #config
     )
 
