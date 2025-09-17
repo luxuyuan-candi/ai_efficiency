b614487832a397ba678b88753343f85fc9d0b267
root <root@localhost.localdomain>
2025-09-17 22:34:44 +0800

    update mcp pod

--- mcp/pod/server.py
+++ mcp/pod/server.py
@@ -6,7 +6,7 @@ mcp = FastMCP("mcp_kubectl_pod")
 @mcp.tool()
 def command_kubectl_pod() -> str:
     """
-    用于查询项目集群对象的pod情况
+    用于查询项目集群对象的异常pod情况
     :return: 最终获得的答案
     """
     command_str = "kubectl get pods --all-namespace | grep -v Running | grep -v Completed"
